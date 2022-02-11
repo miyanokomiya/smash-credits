baca0fa5c72945a0f30d6f2846c56e324691dd22
miyanokomiya <tomoya.hstexze24632@gmail.com>
2022-02-11 22:53:01 +0900

    feat: Fix models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,34 +1,46 @@
+/**
+ * px / frame
+ */
+export type Speed = number;
+
 export interface Credits {
   blockStyle: BlockStyle;
-  titleBlock: TitleBlock;
-  endBlock: EndBlock;
-  creditBlock: CreditBlock;
+  speed: Speed;
+  blockList: BlockBase[];
 }
+
+export const BLOCK_TYPE = {
+  text: "text",
+  credit: "credit",
+} as const;
+export type BlockType = typeof BLOCK_TYPE[keyof typeof BLOCK_TYPE];
 
 export interface BlockBase {
+  type: BlockType;
   blockStyle?: BlockStyle;
+  speed?: Speed;
 }
 
-export interface TitleBlock extends BlockBase {
-  title: string;
-}
-
-export interface EndBlock extends BlockBase {
+export interface TextBlock extends BlockBase {
+  type: "text";
   title: string;
 }
 
 export interface CreditBlock extends BlockBase {
-  name: string;
+  type: "credit";
+  title: string;
   contributors: Contributor[];
 }
 
 export interface Contributor {
-  role: string;
+  role?: string;
   name: string;
 }
 
 export interface BlockStyle {
-  textStyle?: TextStyle;
+  title?: TextStyle;
+  role?: TextStyle;
+  name?: TextStyle;
 }
 
 export interface TextStyle {
